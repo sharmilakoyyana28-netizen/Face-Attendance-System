6bfaa641dd676dd6cdbbf967de3fe769c4665c78
sharmilakoyyana28-netizen <sharmilakoyyana28@gmail.com>
2026-09-26 06:20:53 -0700

    Update app.py

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ if st.button("Register"):
         encodings = face_recognition.face_encodings(image)
         if len(encodings) > 0:
             known_encodings.append(encodings[0])
-            known_names.append(reg
+            known_names.append(reg)
